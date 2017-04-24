61be6e81fef54c7083d0a3671596c0afaf8a6c23
viniciuzvieira <viniciusvieira04@hotmail.com>
2017-04-23 22:13:26 -0300

    Script - Criação de Tabelas
    Script para criação de tabelas

---
+++ criação de tabelas.sql
@@ -0,0 +1,51 @@
+create table pdv(
+    cnpj_pdv int not null primary key,
+    nome_pdv varchar(255) not null
+);
+
+create table funcionario(
+    CPF int not null primary key,
+    Nome varchar(255) not null,
+    Login varchar(255) not null,
+    Senha varchar(255) not null,
+    Cargo varchar(255) not null,
+    Admissao date not null,
+    PDV_funcionario int not null,
+    CTPS varchar(255) not null,
+    enabled varchar(20), 
+    alterado_em date,
+    alterado_por varchar(255)
+);
+
+create table venda(
+    id_venda int not null primary key,
+    Cliente_Cpf varchar(255) not null,
+    pdv_venda varchar(255) not null,
+    Servico varchar(255) not null,
+    Quantidade int not null,
+    Total varchar(255) not null,
+    Data date not null
+);
+
+create table cliente(
+    CPF int not null primary key,
+    Nome varchar(255) not null,
+    Email varchar(255) not null,
+    Telefone varchar(255) not null,
+    Sexo varchar(20) not null,
+    enabled varchar(20) not null,
+    alterado_em date,
+    alterado_por varchar(255)
+);
+
+create table Servico(
+    id_servico int not null primary key,
+    Nome varchar(255) not null,
+    Preco varchar(255) not null,
+    enabled varchar(20) not null,
+    alterado_em date,
+    alterado_por varchar(255)
+);
+
+
+
