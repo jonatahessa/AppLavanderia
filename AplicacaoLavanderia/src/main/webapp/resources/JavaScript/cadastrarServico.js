fbb6c35bd4ccbb37c52e6c3b8244981caddd5b4b
livia.cgsantos <livia.cgsantos@CAS511772W0704.senacsp.edu.br>
2017-04-24 21:40:03 -0300

    cadastrarServico css cadastrarServico js

--- AplicacaoLavanderia/src/main/webapp/resources/JavaScript/cadastrarServico.js
+++ AplicacaoLavanderia/src/main/webapp/resources/JavaScript/cadastrarServico.js
@@ -8,18 +8,10 @@ function validar() {
   var camposvalidos = [];
   var i = 0;
   var j = 0;
-  var id = document.querySelector('#campoIdServico');
   var nome = document.querySelector('#campoNomeServico');
   var preco = document.querySelector('#campoPrecoPeca');
 
 
-  if (id.value == '') {
-    camposinvalidos[i] = "#campoIdServico";
-    i++;
-  } else {
-    camposvalidos[j] = "#campoIdServico";
-    j++;
-  }
   if (nome.value == '') {
     camposinvalidos[i] = "#campoNomeServico";
     i++;
