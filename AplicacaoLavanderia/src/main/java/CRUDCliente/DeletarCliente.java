a41ce7a33a04458598bca98023954f610c356a25
vinicius.vsilva8 <vinicius.vsilva8@CAS511740W0705.senacsp.edu.br>
2017-05-03 21:56:20 -0300

    Deletar Cliente

--- AplicacaoLavanderia/src/main/java/CRUDCliente/DeletarCliente.java
+++ AplicacaoLavanderia/src/main/java/CRUDCliente/DeletarCliente.java
@@ -1,57 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CRUDCliente;
 
+import Daos.DaoCliente;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author vinicius.vsilva8
- * 
- */
+@WebServlet(name = "DeletarCliente", urlPatterns = {"/DeletarCliente"})
 public class DeletarCliente extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        RequestDispatcher dispatcher
-                = request.getRequestDispatcher("cadastroCliente.jsp");
-        dispatcher.forward(request, response);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String nome = request.getParameter("nome");
-        String email = request.getParameter("email");
-        String telefone = request.getParameter("telefone");
-        String cpf = request.getParameter("cpf");
-        String sexo = request.getParameter("sexo");
+        doGet(request, response);
+    }
 
-        Cliente cliente = new Cliente();
-        cliente.setCpf(cpf);
-        cliente.setEmail(email);
-        cliente.setNome(nome);
-        cliente.setTelefone(telefone);
-        cliente.setSexo(sexo);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getServletPath();
 
         try {
-            cliente.deletarCliente(cliente);
-            response.sendRedirect("mensagemExclusao.jsp");
-        } catch (Exception ex) {
-            response.sendRedirect("mensagemErro.jsp");
-            Logger.getLogger(CadastrarCliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            switch (action) {
 
+                case "/update":
+                    updateBook(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void updateBook(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+
+        String title = request.getParameter("cpf");
+
+ /*
+        Cliente cliente = new Cliente(cpf);
+        DAOCliente.updateBook(cliente);*/
     }
 }
+
